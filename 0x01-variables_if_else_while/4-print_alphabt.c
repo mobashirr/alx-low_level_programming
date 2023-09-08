774795bd2ca7d422ffa3d7b385c866688df6a96a
sozoia <zfzg2232@gmail.com>
2023-09-08 15:41:58 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int h = 97;
+
+
+	while (h <= 122 && h != 113 && h !=101);
+	{
+		putchar(h);
+			h++;
+	}
+		putchar('\n')
+
+
+return (0);
+}
+
