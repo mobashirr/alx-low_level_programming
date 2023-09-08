7b12742d1f47954aecaf4dfdc39d9c5f73453ceb
sozoia <zfzg2232@gmail.com>
2023-09-08 15:48:22 -0700

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,12 +10,16 @@ int main(void)
 	int h = 97;
 
 
-	while (h <= 122 && h != 113 && h !=101);
+	while (h >= 122);
+
+	if (h==101 || h==113)
+			h++;
+			continue;
 	{
 		putchar(h);
 			h++;
 	}
-		putchar('\n')
+		putchar('\n');
 
 
 return (0);
