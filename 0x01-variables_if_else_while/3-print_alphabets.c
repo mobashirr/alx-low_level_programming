95c368ee4ca45c92e4eb6e4d988c609ad6ce513d
sozoia <zfzg2232@gmail.com>
2023-09-08 15:06:38 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+        int h = 97;
+	int c = 65
+        while (h <= 122)
+        {
+                putchar(h);
+                        h++;
+        }
+
+	while (c <= 90)
+	{
+                putchar(c);
+			c++;
+	}
+		putchar('\n')
+return (0);
+}
+
