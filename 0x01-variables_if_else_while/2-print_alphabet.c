2b24f215a3ee7ca228a4714eb16a4808b735716b
sozoia <zfzg2232@gmail.com>
2023-09-08 14:34:43 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Discription : charectrt loob
+ * return 0 (success)
+ */
+int main(void)
+{
+	int h ;
+	h = 96
+	
+	while (h < = 122);
+	{
+	putchar("h");
+		ch++;
+	}
+	putchar("\n");
+return (0);
+
+
+}
