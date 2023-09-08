2533569af9a7a9253e237d81da94f161362b1271
sozoia <zfzg2232@gmail.com>
2023-09-07 17:37:03 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that a prints a line the printf function
+ * Return: 0
+ */
+int main(void)
+{
+printf("c languge is profitonal languge,\n");
+return (0);
+}
