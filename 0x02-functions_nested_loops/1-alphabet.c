ff4b552821d30b3f810a358db8a6bf06fac7c0d1
sozoia <zfzg2232@gmail.com>
2023-09-12 09:37:12 -0700

    1-print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*
+ *main - alphabet writer (lower case)
+ *
+ *Return: Always 0 (success)
+ */
+void main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+
+	-putchar('\n')
+
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/*
+ *main - alphabet writer (lower case)
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	print_alphabet();
+return (0);
+}
