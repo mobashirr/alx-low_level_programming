9d163a0589d5ff0ed918de557e1bef021ec286f1
sozoia <zfzg2232@gmail.com>
2023-09-12 08:09:39 -0700

    1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,11 +6,12 @@
  * Return: Always 0 (success)
  */
 
-int main (void)
+int main(void)
 {
 	char str[] = "_putchar";
 	int ch;
-	for (ch = 0; ch <8 ; ch++)
+
+	for (ch = 0; ch < 8 ; ch++)
 		_putchar(str[ch]);
 	_putchar("\n");
 
