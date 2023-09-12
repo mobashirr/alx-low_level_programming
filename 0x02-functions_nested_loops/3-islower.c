b6aa075ccf02488345245cb579f11bb4ba2f3289
sozoia <zfzg2232@gmail.com>
2023-09-12 11:25:26 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,11 @@
 
 /**
  * _islower - check the condetion of the char.
- *
+ *@c: user
  * Return: 1 if the char is lower case other wise it 0.
  */
 
-	int _islower()
+	int _islower(int c)
 	{
 		if (c >= 'a' && c <= 'z')
 			return (1);
