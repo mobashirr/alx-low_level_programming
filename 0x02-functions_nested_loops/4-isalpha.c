eb9bf40db2f6cba00379675c68b14a04c11838f6
sozoia <zfzg2232@gmail.com>
2023-09-12 11:44:47 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * _isalpha - checks alphabets.
  *
  * @c : is the char
- * REturn; 1 if its a char only 
+ * REturn; 1 if its a char only other wise its 0
  */
 
 
@@ -12,4 +12,3 @@
 	{
 		return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 	}
-
