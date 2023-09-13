d1e7ecc964f25a88a03a3c3e36d848815cd7fa4d
sozoia <zfzg2232@gmail.com>
2023-09-13 11:48:04 -0700

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
+/**
+ * main.h - is like a header file.
+ *@i : int
+ * Return: depend on the fun you want.
+ */
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
