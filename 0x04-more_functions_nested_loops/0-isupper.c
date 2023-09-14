e1a0302044f19bf00fc8248d6eb1bd554df3672d
sozoia <zfzg2232@gmail.com>
2023-09-14 13:54:50 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,8 +9,6 @@
 int _isupper(char c)
 
 {
-/*for this algorithm you need to return 1 only when char (c) is uppercase */
-
 	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,7 +5,6 @@
 *void prtp (void)
 *int prtp (int)
 */
-
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
