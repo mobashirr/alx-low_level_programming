06f41f617b1f7d4a819ad5d61e7722d3e2386762
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-09-14 23:26:34 +0300

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,14 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 /**
- * main.h - is like a header file.
- *@i: int
- * Return: depend on the fun you want.
- */
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+*main - is like aheader file for prtp
+*void prtp (void)
+*int prtp (int)
+*/
+
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -16,6 +13,10 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void print_number(int n);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
 
-
-#endif /* MAIN_H */
+#endif /*complete*/
