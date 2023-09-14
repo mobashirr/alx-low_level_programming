e3a61520f547aaa37b10d5b84f5840eadff80045
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-09-14 23:32:30 +0300

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,24 +2,17 @@
 #include "main.h"
 /**
  * _isupper - check if char is upper case or not
- *@c; is char
+ *@c: is char
  * Return: char(c)
  */
 
 int _isupper(char c)
 
 {
+/*for this algorithm you need to return 1 only when char (c) is uppercase */
 
 	if (c >= 'A' && c <= 'Z')
-
-	{
 		return (1);
-	}
-
 	else
-
-	{
-			return (0);
-	}
-
+		return (0);
 }
