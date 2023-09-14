a2c94e38832b34bd72c03320b39846329f904f01
sozoia <zfzg2232@gmail.com>
2023-09-14 12:52:42 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isupper - check if char is upper case or not
+ *@c; is char
+ * Return: char(c)
+ */
+
+int _isupper(char c)
+
+{
+	char c;
+
+	while ()
+	{
+
+		if (c = < 65 && c = > 90)
+		{
+			c = 1;
+		}
+	else
+	{
+			c = 0;
+	}
+
+	}
+	return (c);
+}
