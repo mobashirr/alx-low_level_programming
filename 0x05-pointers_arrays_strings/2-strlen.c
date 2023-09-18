93560b763f58c6e41f722cd49af2f302dc244ba8
sozoia <zfzg2232@gmail.com>
2023-09-18 11:15:27 -0700

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * int _strlen - length of string
+ * @s : string
+ *Return:  returns the length of a string.
+ */
+
+int _strlen(char *s)
+{
+size_t strlen(const char *s);
+}
