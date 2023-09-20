c91f7e6c57124cb8430894626c22f3cb7fcce65c
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-09-20 21:11:05 +0300

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-if (*s1 != *s2)
+
+  while (*S1 != '\0' && *s2 != '\0' && *s1 == *s2)
+  {
+    ++s1;
+    ++s2;
+  }
+  
+if (s1 != *s2)
 {
 return (*s1 - *s2);
 }
