c00bd2f2ea8fac9278800cd4280c787a39e9206e
sozoia <zfzg2232@gmail.com>
2023-09-20 01:52:50 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - function to copy string
+ *@dest: string
+ *@src: string
+ *@n: is byte size
+ *Return: char * to pointer
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int a = 0;
+int b;
+
+while (dest[a] != '\0' && a < n)
+{
+	dest[a] = src[a];
+	++a;
+}
+
+return (dest);
+}
