c209df0a02d918dc30dff92bc6d1b61e299c78ea
sozoia <zfzg2232@gmail.com>
2023-09-19 23:32:19 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - oncatenates two strings.
+ *@dest: first string
+ *@src: second string
+ *Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+int a;
+int b;
+
+a = 0;
+b = 0;
+
+while (dest[a] != '\0')
+{
+	++a;
+}
+
+while (src[b] != '\0')
+{
+dest[a] = src[b];
+}
+
+return (dest);
+
+}
