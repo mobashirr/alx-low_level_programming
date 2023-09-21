363fe7375493394c2cbc0906d5745da36428e0d9
sozoia <zfzg2232@gmail.com>
2023-09-21 04:06:58 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -18,27 +18,27 @@ while (s[a] != '\0')
 
 if (s[a] == 'a' || s[a] == 'A')
 {
-s[a] = 4;
+s[a] = '4';
 }
 
 else if (s[a] == 'e' || s[a] == 'E')
 {
-s[a] = 3
+s[a] = '3';
 }
 
 else if (s[a] == 'o' || s[a] == 'O')
 {
-s[a] = 0;
+s[a] = '0';
 }
 
 else if (s[a] == 't' || s[a] == 'T')
 {
-s[a] = 7;
+s[a] = '7';
 }
 
 else if (s[a] == 'l' || s[a] == 'L')
 {
-s[a] = 1;
+s[a] = '1';
 }
 
 ++a;
