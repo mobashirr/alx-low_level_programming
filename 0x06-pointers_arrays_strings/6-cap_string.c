3496e9bc67092df837ab8bd54bbaef4d21cc9884
sozoia <zfzg2232@gmail.com>
2023-09-21 05:20:58 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * cap_string - capitalizes most of the words in a string.
+ * @s: analized string.
+ * Return: String with all words capitalized.
+ */
+char *cap_string(char *s)
+{
+	int a, g;
+	int m] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+
+a = 0;
+while (*(s + a) != '\0')
+{
+	if (*(s + a) >= 'a' && *(s + a) <= 'z')
+	{
+		if (a == 0)
+		{
+				*(s + a) = *(s + a) - 32;
+		}
+			
+			else
+			{
+				for (g = 0; g <= 12; g++)
+				{
+					if (m[g] == *(s + a - 1))
+					{
+						*(s + a) = *(s + a) - 32;
+					}
+				}
+			}
+		}
+	a++;
+	}
+	return (s);
+}
