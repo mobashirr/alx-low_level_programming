d1653cf2a4b2e7ab61829474346569e9857beb0b
sozoia <zfzg2232@gmail.com>
2023-09-25 11:02:45 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _memset - function fill with b
+ *@s: array 
+ *@b: will be filled with
+ *@n: the num of elment to fill
+ *Return: char*
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+int i;
+int a;
+
+for (i = 0; i < n; ++i)
+{
+s[i] = b;
+}
+
+
+
+
+
+return (s);
+}
+
