715c35f8366cca946f34c5efc336d5f5274aac54
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-09-25 23:58:29 +0300

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+  * _strst - locates a substring.
+  *@haystack: main string
+  *@needle: string to search
+  *Return: new string
+  */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		if ((*haystack == *needle && coincidence(haystack, needle) == 1) || !*needle)
+		{
+			return (haystack);
+		}
+		else
+		{
+			haystack++;
+		}
+	}
+	return (0);
+}
+/**
+  * coincidence - define if the string d id in c or not
+  * @c: main string.
+  * @d: string that will search for
+  * Return: 1 or 0
+  */
+int coincidence(char *c, char *d)
+{
+	while (*d && *d == *c)
+	{
+		d++;
+		c++;
+	}
+
+	if (*d == '\0')
+		return (1);
+	else
+		return (0);
+}
