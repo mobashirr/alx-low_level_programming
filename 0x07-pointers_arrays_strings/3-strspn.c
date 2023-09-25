c2aa4161e3eadfee380ea110e35c64f8f8066dca
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-09-25 23:27:35 +0300

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,9 +1,33 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * _strspn - 
+ *@s: string
+ *@accept: acception
+ *Return: unsigned in
  */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a;
+  unsigned int b;
+
+
+for (a = 0; s[a] != '\0'; a++)
+{
+b = 0;
+
+for (; accept[b] != s[a]; b++)
+{
+if (accept[b] == '\0')
+return (a);
+}
+
+	}
+
+	return (a);
+}
+
+
 
 
