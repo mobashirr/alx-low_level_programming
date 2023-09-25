c9e68cd3c4082cab2736a03da78e2435171491c8
sozoia <zfzg2232@gmail.com>
2023-09-25 11:47:55 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,26 @@
+#inclde "main.h"
+
+/**
+ * _memcpy - copy elements from dest to src
+ *@dest: string
+ *@src: string
+ *@n: num of elements wanted
+ *Return: return char*
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+	int b;
+
+	for (a = 0; a < n; a++)
+	{
+	for (b = 0; src[b] != '\0'; b++)
+	{
+	dest[a] = src[b];
+	}
+
+	}
+
+	return (dest);
+}
