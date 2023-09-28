fe6efc3bd62f88847ee026035028eb7feb98151c
sozoia <zfzg2232@gmail.com>
2023-09-28 05:31:31 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,15 +6,18 @@
  *Return: char to pointer
  */
 
-char _puts_recursion(char *s)
+void _puts_recursion(char *s)
 {
 if (*s == '\0')
 {
 	_putchar('\n');
-	return;
 }
 
+if (*s != '\0')
+{
 _putchar(*s);
 
 _puts_recursion(s + 1);
+}
+
 }
