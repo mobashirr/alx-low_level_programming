8020257a9c0634f6fbd54a94e645173f1c4458da
sozoia <zfzg2232@gmail.com>
2023-09-28 08:08:55 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+
+/**
+ * _pow_recursion - the result of x to the power of y.
+ *@x: the base num
+ *@y: the power num
+ *Return: int
+ */
+
+int _pow_recursion(int x, int y)
+{
+
+int sum = 0;
+
+if (y > 0)
+{
+sum = x * _pow_recursion(x, y - 1)
+return (sum);
+}
+
+if (y == 0)
+{
+return (1);
+}
+
+
+if (y < 0)
+{
+sum = -x * _pow_recursion(x, y - 1);
+return (sum);
+}
+
+
+return (sum);
+
+}
+
