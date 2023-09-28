73b99975406505a797562db205315eaa16b168f0
sozoia <zfzg2232@gmail.com>
2023-09-28 13:50:10 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a numbe.
+ *@n: the numbear
+ *Return: int
+ */
+
+int _sqrt_recursion(int n)
+{
+int a = 0;
+
+
+
+
+
+}
