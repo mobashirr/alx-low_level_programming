dd2ad19c332f4cfec7d3d556c50ad491bc2eb429
sozoia <zfzg2232@gmail.com>
2023-09-28 07:29:32 -0700

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * factorial - find factorial of num
+ *@n: passed num.
+ *Rrturn: int
+ */
+
+int factorial(int n)
+{
+int sum = 0;
+
+if (n < 0)
+	{
+	return (-1);
+	}
+if (n > 0)
+{
+sum = n * factorial(n - 1);
+return (sum);
+}
+if (n == 0)
+{
+	return (1);
+}
+
+
+}
