40082c2720f82651fee07a438d8704336184e7fd
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-09-28 23:53:25 +0300

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,29 @@
 #include "main.h"
 
 /**
+ * sqrt_check - check the square root.
+ * @n: Number to find square root of
+ * @l: The number checked to see if it's the sqrt of n
+ *
+ * Return: The square root value
+ */
+int sqrt_check(int n, int c)
+{
+	if  (c * c == n)
+	{
+		return (c);
+	}
+	else if (c >= n / 2)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (sqrt_check(n, c + 1));
+	}
+}
+
+/**
  * _sqrt_recursion - returns the natural square root of a numbe.
  *@n: the numbear
  *Return: int
@@ -8,10 +31,20 @@
 
 int _sqrt_recursion(int n)
 {
-int a = 0;
-
-
-
-
-
+if (n == 1)
+	{
+		return (1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (sqrt_check(n, 1));
+  }
 }
