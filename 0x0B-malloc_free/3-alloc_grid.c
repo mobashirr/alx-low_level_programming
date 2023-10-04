98c4508238f76496b54d66e4006f14f15432e14a
sozoia <zfzg2232@gmail.com>
2023-10-04 14:48:29 -0700

    3-allocate_gride.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,38 +13,32 @@ int a = 0, b = 0;
 int **arr;
 if (width == 0 || height == 0)
 {return (NULL); }
-
 arr = malloc(sizeof(*arr) * height);
-
 if (arr == NULL)
 {
 free(arr);
 return (NULL);
 }
-
 while (a < height)
 {
 arr[a] = (int *)malloc(width * sizeof(int));
 if (arr[a] == NULL)
+{ a = 0;
+while (a < height)
 {
-a = 0;
-while (a < height){
-free(arr[a]); ++a; }
+free(arr[a]);
+++a; }
 free(arr);
 return (NULL);
 }
 ++a;
 }
-
 a = 0;
-
 while (a < height)
 {
 b = 0;
-
 while (b < width)
-{
-arr [a][b] = 0;
+{ arr [a][b] = 0;
 ++b;
 }
 ++a;
