3bec22ad8c8aeee3a420eb0fd7e0055c993218ff
sozoia <zfzg2232@gmail.com>
2023-10-04 13:32:08 -0700

    alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * alloc_grid - make tow dimantional array
+ *@width: row
+ *@height: colomon
+ *Return: pointer to pointer.
+ */
+
+int **alloc_grid(int width, int height)
+{
+int **arr = (int **)malloc(height * sizeof(int *));
+
+int a = 0;
+int b = 0;
+
+while (a < width)
+{
+arr[a] = (int *)malloc(height * sizeof(int));
+++a;
+}
+
+a = 0;
+while (a < width)
+{
+
+b = 0;
+while (b < height)
+{
+
+arr [a][b] = 0;
+++b;
+}
+
+++a;
+}
+
+return (arr);
+}
