2629bd69df938d3decc7e000d49f7df4b68eb0f8
sozoia <zfzg2232@gmail.com>
2023-10-06 13:52:00 -0700

    do it all alone

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program that prints multiplication of argument passed
+ *@argc: count
+ *@argv: string
+ *Return: int
+ */
+
+int main(int argc, char **argv)
+{
+int a = argc;
+int b = 1;
+int sum = 1;
+int nu = 0;
+
+if (a != 3)
+{
+printf("Error\n");
+return (0);
+}
+
+while (a > b)
+{
+
+nu = atoi(argv[b]);
+
+sum = sum *nu;
+++b;
+}
+
+printf("%d\n", sum);
+
+return (0);
+}
