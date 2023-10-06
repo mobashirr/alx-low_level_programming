183a9592391846f53befc1545f6ab291af4dd383
sozoia <zfzg2232@gmail.com>
2023-10-06 09:39:19 -0700

    malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *malloc_checked - reserve memory
+ *@b: number of bytes for allocated memory
+ *Return: void pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+void *mem = malloc(b);
+
+if (mem == NULL)
+{
+
+	exit(98);
+}
+
+return (mem);
+
+
+}
