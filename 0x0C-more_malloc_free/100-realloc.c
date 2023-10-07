25e8dcd228d0ebf800ce0bbffc609a4e4277a98f
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-10-07 18:13:56 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -7,47 +7,47 @@
  *@new_size: size to be reallocated ptr for.
  *Return: void pointer
  */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size) {
+    if (old_size == new_size) {
+        return ptr;
+    }
 
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-{
-void *reallo;
+    if (new_size == 0 && ptr != NULL) {
+        free(ptr);
+        return NULL;
+    }
 
-if (old_size == new_size)
-{
-	return (ptr);
+    if (ptr == NULL && new_size != 0) {
+        void *reallo = malloc(new_size);
+
+        if (reallo == NULL) {
+            return NULL;
+        }
+
+        return reallo;
+    }
+
+    void *reallo = malloc(new_size);
+
+    if (reallo == NULL) {
+        free(ptr);
+        return NULL;
+    }
+
+    memcpy(reallo, ptr, (old_size < new_size) ? old_size : new_size);
+    free(ptr);
+
+    return reallo;
 }
 
-if (new_size == 0 && ptr != NULL)
-{
-	free(ptr);
-	return (NULL);
-}
+int main() {
+    int *arr = malloc(5 * sizeof(int));
+    printf("Old address: %p\n", arr);
 
-if (ptr == NULL && new_size != 0)
-{
-	reallo = malloc(new_size);
+    int *new_arr = _realloc(arr, 5 * sizeof(int), 10 * sizeof(int));
+    printf("New address: %p\n", new_arr);
 
-	if (reallo == NULL)
-		return (NULL);
+    free(new_arr);
 
-	strncpy(reallo, ptr, new_size);
-	return (reallo);
-}
-
-
-	reallo = malloc(new_size);
-
-	if (reallo == NULL)
-	{
-		free(reallo);
-		return (NULL);
-	}
-
-
-
-		strncpy(reallo, ptr, new_size);
-		free(ptr);
-
-		return (reallo);
-
+    return 0;
 }
