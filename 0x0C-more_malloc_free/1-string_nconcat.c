99590e62a3edf4562bd7797a484cb53445dfbff9
sozoia <zfzg2232@gmail.com>
2023-10-07 01:07:39 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *string_nconcat - reserve memory
+ *string_nconcat - reserve memory for two str and concatenate them.
  *@n: number of bytes for to concatenate from s2
  *@s1: string1.
  *@s2: string2.
@@ -13,7 +13,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 char *con;
 int len;
 unsigned int len2 = 0;
-char *str2 = (char*)malloc(sizeof(char) * n);
+char *str2 = (char*)malloc(sizeof(char) * n + 1);
+
+if (str2 == NULL)
+{
+	free(str2);
+	return (NULL);
+}
 
 if (s1 == NULL)
 	s1 = "";
@@ -21,7 +27,8 @@ if (s1 == NULL)
 if (s2 == NULL)
 	s2 = "";
 
-while (len2 < n)
+
+while (len2 < n && s2[len2] != '\0')
 {
 	str2[len2] = s2[len2];
 	++len2;	}
@@ -32,6 +39,7 @@ while (len2 < n)
 	if (con == NULL)
 	{
 	free(con);
+	free(str2);
 	return (NULL);
 	}
 
