fcedf743546ee9f32a02882922de336a880d7852
sozoia <zfzg2232@gmail.com>
2023-10-10 10:39:15 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include "dog.h"
+
+/**
+ * init_dog - a
+ *@d
+ *@name:name
+ *@age: age
+ *@owner: owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+d->name = strdup(name);
+d->age = age;
+d->owner = strdup(owner);
+
+
+}
