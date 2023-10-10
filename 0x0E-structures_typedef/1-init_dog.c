613587b40620a67a5ee88f8fc59610bd222ef656
sozoia <zfzg2232@gmail.com>
2023-10-10 11:07:24 -0700

    solve problems

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,10 +12,11 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
+if (d != NULL)
+{
 d->name = strdup(name);
 d->age = age;
 d->owner = strdup(owner);
+}
 
-free(d->name);
-free(d->owner);
 }
