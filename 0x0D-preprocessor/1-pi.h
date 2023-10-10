bc65fef841cb1c20786bc2d36460069e8e155f2a
sozoia <zfzg2232@gmail.com>
2023-10-10 05:22:58 -0700

    task 1

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -2,8 +2,8 @@
 #define MYMAC_H
 
 /**
- * macro named PI as an abbreviation for the token 3.14159265359.
- * 
+ * PI - as an abbreviation for the token 3.14159265359.
+ *
  */
 
 #define PI 3.14159265359
