3953fbbcf2464548776c2caa7416a91b35f95422
sozoia <zfzg2232@gmail.com>
2023-10-10 13:01:07 -0700

    task4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include <stdio>
+#include "dog.h"
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+int *p;
+
+
+dog_t *p;
+
+
+if (name != NULL && owner != NULL)
+{
+p->name = name;
+p->age = age;
+p->owner = owner;
+}
+
+
+return(p);
+}
