da0f2f052b9bd440663aa22885c2727f61bf80bb
sozoia <zfzg2232@gmail.com>
2023-10-10 11:23:27 -0700

    task2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+ *  print_dog - a
+ *@d: pointer.
+ */
+
+void print_dog(struct dog *d)
+{
+
+if (d != NULL)
+{
+
+printf("Name: %s\n", d->name);
+printf("Age: %f\n", d->age);
+printf("Owner: %s\n", d->owner);
+}
+
+}
