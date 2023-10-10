c951dbdc7294da0ce44cdb859dcc5a3b68004bad
sozoia <zfzg2232@gmail.com>
2023-10-10 10:56:41 -0700

    solve problems

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,6 +8,7 @@
  *@owner: the owner of the dog
  */
 
+
 struct dog
 {
 
@@ -18,9 +19,6 @@ char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 
 
