bd57e4e352251b7edb9d0af0aeda40c88a42274a
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-10-12 01:10:29 +0300

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,27 +2,29 @@
 
 /**
  *get_op_func - chose the op
+ *@s: sign of op
  *Return: pointer to func
  */
 int (*get_op_func(char *s))(int, int)
 {
-op_t ops[]={
-	{"+",op_add},
-	{"-",op_sub},
-	{"*",op_mul},
-	{"/",op_div},
-	{"%",op_mod},
-	{NULL,NULL}
+op_t ops[]=
+{
+{"+",op_add},
+{"-",op_sub},
+{"*",op_mul},
+{"/",op_div},
+{"%",op_mod},
+{NULL,NULL}
 };
 int i = 0;
 
-	while (i < 5)
-	{
-		if (s[0] == *(ops[i].op))
-		{
-			return(ops[i].f);
-		}
-		++i;
-	}
-		return (NULL);
+while (i < 5)
+{
+if (ops[i].op[0] == *s)
+{
+return(ops[i].f);
+}
+++i;
+}
+return (NULL);
 }
