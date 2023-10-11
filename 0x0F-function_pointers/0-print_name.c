bb759961164bbfedfcadb7afa5a132e7a9b716a7
sozoia <zfzg2232@gmail.com>
2023-10-11 09:40:15 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ *print_name - call pointer to function
+ *@name: the name
+ *@f: pointer to fun
+ *Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+
+f(name);
+
+}
