039c6c24a401313df9afdaa139234d5ba8b46c92
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-10-12 01:28:47 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,10 +1,12 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- *
- *
- *
+ * main - op program
+ *@argc: c
+ *@argv: a
+ *Return: int
  */
 
 int main(int argc, char *argv[])
@@ -13,7 +15,6 @@ int main(int argc, char *argv[])
 
 int num1 ;
 int num2 ;
-int (*res) (int, int);
 int final;
 
 if (argc != 4)
@@ -25,9 +26,13 @@ exit (98);
 num1 = atoi(argv[1]);
 num2 = atoi(argv[3]);
 
-res = get_op_func(argv[2]);
+final = (get_op_func(argv[2])) (num1,num2);
 
-if (res == NULL || strlen(argv[3]) != 1)
+if (((*argv[2] != '+') &&
+(*argv[2] != '-') &&
+(argv[2] != '*') &&
+(*argv[2] != '%') ||
+strlen(argv[3]) != 1)
 {
 printf("Error\n");
 exit (99);
@@ -39,7 +44,7 @@ printf("Error\n");
 exit (100);
 }
 
-final = res(num1, num2);
+
 printf("%d\n",final);
 
 return (0);
