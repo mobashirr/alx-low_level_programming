ab9c4348fffb03864655c7331c9f27180a38f2f7
sozoia <zfzg2232@gmail.com>
2023-10-11 11:28:40 -0700

    task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,34 @@
+#include "function_pointers.h"
+
+/**
+ *int_index - call pointer to function for each elment of the array
+ *@array: the name of the array
+ *@cmp: pointer to fun
+ *@size: size of the array
+ *Return: int
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	size_t co = 0;
+	int result = 0;
+
+	if (size <= 0)
+	return (0);
+
+		if (cmp != NULL)
+		{
+		while (co < size)
+		{
+		result = cmp(array[co]);
+
+		if (result == 1)
+		return (co);
+
+		++co;
+		}
+
+		}
+
+	return (result);
+}
