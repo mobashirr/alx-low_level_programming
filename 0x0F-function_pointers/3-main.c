5ce03c5784052780c7681e85b536f72c5589b38b
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-10-12 02:18:51 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -38,7 +38,7 @@ printf("Error\n");
 exit(99);
 }
 
-if ((*argv[2] == '/' || *argv[3] == '%') && (num2 == 0))
+if ((*argv[2] == '/' || *argv[2] == '%') && (num2 == 0))
 {
 printf("Error\n");
 exit(100);
