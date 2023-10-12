22fcf197353d8e8245742f75ee9e496189f5d356
sozoia <zfzg2232@gmail.com>
2023-10-12 04:43:57 -0700

    main.hwq

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,13 @@
-#ifndef variadic_functions.h
-#define variadic_functions.h
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
 
-
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
