77037a803dcbe1d53f1081b7ee8a3dbb7447cb47
sozoia <zfzg2232@gmail.com>
2023-10-12 06:35:51 -0700

    task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print nums
+ *@separator: between num
+ *@n: num of para
+ *Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+int num = 0, c;
+va_list para;
+va_start(para, n);
+
+
+
+for (c = 0; c < (int)n; ++c)
+{
+	num  = va_arg(para, int);
+	printf("%i", num);
+
+	if (separator != NULL && (c + 1) != (int)n)
+	printf("%s", separator);
+}
+
+printf("\n");
+
+va_end(para);
+
+}
