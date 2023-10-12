9f720f79fd314838ef93ff61a58626e76e48b1f9
sozoia <zfzg2232@gmail.com>
2023-10-12 07:27:16 -0700

    task 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print strings
+ *@separator: between strings
+ *@n: num of para
+ *Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+char *str, *hand;
+int c;
+va_list para;
+va_start(para, n);
+
+
+
+for (c = 0; c < (int)n; ++c)
+{
+	hand = va_arg(para, char*);
+	str  = (char*)hand =! NULL ? hand : "(nil)";
+	printf("%s", str);
+
+	if (separator != NULL && (c + 1) != (int)n)
+	printf("%s", separator);
+}
+
+printf("\n");
+
+va_end(para);
+
+}
