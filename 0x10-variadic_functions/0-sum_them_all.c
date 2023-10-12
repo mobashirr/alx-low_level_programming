3ddb0f42c10ccc9bcdba33dab9f916c3f5d01b14
sozoia <zfzg2232@gmail.com>
2023-10-12 04:40:28 -0700

    readme.md

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ *sum_them_all - find some of multy para
+ *@n: num of para
+ *Return: sum of all para
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+int result = 0;
+int s = 0;
+
+va_list para;
+va_start(para, n);
+
+if (n == 0)
+	return (0);
+
+	while (n > s)
+	{
+	result = result + va_arg(para, int);
+		++s;
+	}
+	va_end para;
+
+			return (result);
+}
