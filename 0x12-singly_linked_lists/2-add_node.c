2dd244ed8a3ed20c871a85cc47138d7818b95a54
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-10-19 22:55:10 +0000

    task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_node - function to add node to linked list
+ * @head: pointer from type size_t 
+ * @str: string to add.
+ * Return: pointer to node cteated
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *node;
+
+node = malloc(sizeof(list_t));
+if (!node)
+{
+ return NULL;
+}
+
+node->str = strdup(str);
+if (!node->str)
+{
+free(node); 
+return NULL;
+}
+
+node->len = _strlen(str);
+node->next = *head;
+
+*head = node;
+return (node);
+}
