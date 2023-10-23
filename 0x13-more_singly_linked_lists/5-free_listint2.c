4b6d11130b9a0135379a6ea44ae32cf2a91be701
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-10-23 17:01:07 +0000

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * free_listint2 - free linked list elements and set 
+ * free_listint2 - free linked list elements and set
  * head to NULL value.
  * @head: pointer to the head of the list
 */
@@ -9,12 +9,12 @@ void free_listint2(listint_t **head)
 {
 
 listint_t *next;    /*set pointer as temp*/
-                    /*the head now is ptr to ptr to struct */
-	while (*head)             /*we can derefrense between poiner to pointer*/
+
+	while (*head)             /*we can derefrense between ptr to ptr to struct*/
 	{
-		if ((*head)->next)      /*this way derefrense work*/
-		{                            /*the logic is:*/
-			next = (*head)->next;      /* 1- save address of next node if there*/
+		if ((*head)->next)      /*the head is ptr to ptr to struct */
+		{
+			next = (*head)->next;     /*this way derefrense work*/
 			free(*head);             /* 2- free the current node */
 			*head = next;            /* 3- go to the next node*/
 		}
@@ -23,6 +23,5 @@ listint_t *next;    /*set pointer as temp*/
 			free(*head);
 			break; }
 	}
-    
-    *head = NULL;
+		*head = NULL;
 }
