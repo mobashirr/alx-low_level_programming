e9e99bdf0af5c87c1b070acca8c96040498f064b
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-10-23 20:21:31 +0000

    task 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position.
+ * @head: linked list
+ * @idx: index of the list where the new node should be added
+ * @n: the number that should be added
+ * Return: the address of the new node, or NULL if it failed
+ * if it is not possible to add the new node at index idx,
+ * do not add the new node and return NULL.
+*/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *node, *next, *cur;
+
+	if (!*head)
+	return (NULL);
+
+	node = malloc(sizeof(listint_t));
+	if (!node)
+	return (NULL);
+	node->n = n;
+
+	cur = *head;
+	for (i = 0; (i + 1) != idx; i++)
+	{	/*we want to reach the elment before  number idx*/
+		if (cur->next)
+		cur = cur->next;
+		else
+		break;
+	}
+	if ((i + 1) == idx)
+	{
+		next = cur->next;
+		cur->next = node;
+		node->next = next;
+		return (node);
+	}
+	else if (idx == 0)
+	{
+	node->next = *head;
+	*head = node;
+	return (node); }
+	return (NULL);
+}
