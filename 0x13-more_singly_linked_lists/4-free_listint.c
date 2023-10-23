f7125b638ca93b082c43495294d412d81862d809
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-10-23 16:37:27 +0000

    task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,7 +7,20 @@
 
 void free_listint(listint_t *head)
 {
+listint_t *next;    /*set pointer as temp*/
 
-    
+	while (head)
+	{
+		if (head->next)
+		{                            /*the logic is:*/
+			next = head->next;      /* 1- save address of next node if there*/
+			free(head);             /* 2- free the current node */
+			head = next;            /* 3- go to the next node*/
+		}
+		else
+		{
+			free(head);
+			break; }
+		}
 
 }
