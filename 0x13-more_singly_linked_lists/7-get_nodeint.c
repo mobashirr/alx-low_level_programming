d0633491500e4dfe2a25109ac24fd4711c09eb82
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-10-23 18:33:10 +0000

    task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - this funtion give you
+ * choise which node you want
+ * @head: linked list.
+ * @index: the number of node
+ * Return: the number (data) in the chosen node
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *node;
+
+	for (i = 0; i != index && head; i++)
+	{
+		if ((head)->next)
+		head = (head)->next;
+	}
+	node = head;
+	return (node);
+}
