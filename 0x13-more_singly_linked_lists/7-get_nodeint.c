2ca62289bfaebe7d4bb7e779e10439ab0a62a58e
sozoia <zfzg2232@gmail.com>
2023-10-25 00:10:57 +0300

    task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *node;
 
 		if (!head)
-		return(NULL);
+		return (NULL);
 
 	for (i = 0; i != index && head; i++)
 	{
