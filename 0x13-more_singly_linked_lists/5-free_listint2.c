3abf2bb4e589c01cd5f734b562f0826b24837e3e
sozoia <zfzg2232@gmail.com>
2023-10-24 23:27:23 +0300

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,19 +7,18 @@
 */
 void free_listint2(listint_t **head)
 {
-listint_t *next, *cur;    /*set pointer as temp*/
+listint_t *next;    /*set pointer as temp*/
 
 	if (!*head)
-		exit(98);
+		return;
 
 	while (*head)             /*we can derefrense between ptr to ptr to struct*/
 	{
 		if ((*head)->next)      /*head is ptr to ptr to struct */
 		{
 			next = (*head)->next;    /*this way derefrense work*/
-			cur = *head;			/*save the curent address to free it*/
+			free(*head);			/*free the current node*/
 			*head = next;           /*go to the next node*/
-			free(cur);             /* free the current node */
 		}
 		else
 		{
@@ -27,6 +26,4 @@ listint_t *next, *cur;    /*set pointer as temp*/
 			break; }
 	}
 		*head = NULL;
-		cur = NULL;
-		next = NULL;
 }
