25be2eb34e59808293b2b24c715feaa4e4537226
sozoia <zfzg2232@gmail.com>
2023-10-25 00:49:33 +0300

    task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,47 +1,47 @@
 #include "lists.h"
 /**
- * delete_nodeint_at_index -
- * @head:
- * @index:
- * Return: 
+ * delete_nodeint_at_index - delete node
+ * @head: linked list
+ * @index: number of node
+ * Return: 1 in success or -1 if not
 */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    unsigned int i;
-    listint_t *temp, *cur;
+	unsigned int i;
+	listint_t *temp, *cur;
 
-    if (!*head || !head)
-    return (-1);
+	if (!*head || !head)
+	return (-1);
 
-     if (index == 0)
-    {
-        cur = *head;
-        *head = (*head)->next;
-        free(cur);
-        return (1);
-    }
+	 if (index == 0)
+	{
+		cur = *head;
+		*head = (*head)->next;
+		free(cur);
+		return (1);
+	}
 
-    cur = *head;
-    for (i = 0; (i + 1) != index; i++)
-    {
-        if (cur->next)
-        cur = cur->next;
-        else
-        break;
-    }
+	cur = *head;
+	for (i = 0; (i + 1) != index; i++)
+	{
+		if (cur->next)
+		cur = cur->next;
+		else
+		break;
+	}
 
-    if (cur->next->next && (i + 1) == index)
-    {
-        temp = cur->next->next;
-        free(cur->next);
-        cur->next = temp;
-        return (1);
-    }
-    else if (!cur->next->next && (i + 1) == index)
-    {
-        free(cur->next);
-        return (1);
-    }
+	if (cur->next->next && (i + 1) == index)
+	{
+		temp = cur->next->next;
+		free(cur->next);
+		cur->next = temp;
+		return (1);
+	}
+	else if (!cur->next->next && (i + 1) == index)
+	{
+		free(cur->next);
+		return (1);
+	}
 
-    return (-1);
+	return (-1);
 }
