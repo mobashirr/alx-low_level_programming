27215a23aff4bfc41a04aee607167209faa1e693
sozoia <zfzg2232@gmail.com>
2023-10-25 00:28:10 +0300

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,10 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		next = cur->next;	/*save the next node*/
 		cur->next = node;	/*put the new node at the position*/
 		node->next = next;	/*link the node*/
-		if (node->next)
 		return (node);
-		else
-		return (NULL);
 	}
 	else if (idx == 0)
 	{
