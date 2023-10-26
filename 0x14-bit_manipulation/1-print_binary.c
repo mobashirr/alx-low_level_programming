ddca8234a951621cf6ec803cf1a1fa237335f9e4
sozoia <zfzg2232@gmail.com>
2023-10-27 00:07:07 +0300

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,7 +10,12 @@ void print_binary(unsigned long int n)
 	if (n > 1)
 	print_binary(n >> 1);
 
+/*the & opreator when using it with number and one (1)*/
+/*one is like this when compare with int: */
+/* 0000 0000 0000 0001*/
+/*so alyaws take only the first number wich 1*/
 	num = n & 1;		/*set the last digit*/
+
 	_putchar(num + '0');
 
 }
