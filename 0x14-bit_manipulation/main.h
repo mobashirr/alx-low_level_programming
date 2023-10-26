aa663d27ebff15b62ef9850f813f77b6eb0e99ef
sozoia <zfzg2232@gmail.com>
2023-10-26 19:28:49 +0300

    task 1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,11 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
-
+/*i used this helper functions*/
+void print_buf(char *buf,int n);
+int _putchar(char c);
 
 #endif
