39b32924f3a014298aac8a1267011ca185e4b6ee
sozoia <zfzg2232@gmail.com>
2023-10-27 01:22:33 +0300

    advansed 1

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness.
+ * Return: int
+*/
+int get_endianness(void)
+{
+	int num = 0x01;
+	char *str;
+
+	str = (char *)&num;
+	if(str == 0x01)
+	return (1);
+
+	return (0);
+}
