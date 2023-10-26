6122012887b820da9a5294388505d8a4abd12b7b
sozoia <zfzg2232@gmail.com>
2023-10-27 00:02:23 +0300

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,42 +1,16 @@
 #include "main.h"
-
-/**
- * print_buf - print the buffer in reversed order
- * @buf: buffer contain binary number
- * @n: the end of the number
-*/
-void print_buf(char *buf,int n)
-{
-	while (n >= 0)
-	{
-		_putchar(buf[n]);
-		n--;
-	}
-}
-
 /**
  * print_binary - print numbers in binary form
  * @n: number to be printed
 */
 void print_binary(unsigned long int n)
 {
-	int i,rest;
-	char *ptr;
-	unsigned long num = n;
+	int num;
 
-	ptr = malloc(sizeof(char) * n);
-	if (!ptr)
-	{
-		exit(98);
-	}
+	if (n > 1)
+	print_binary(n >> 1);
 
-	for (i = 0; num != 0; i++)
-	{
-		rest = num % 2;
-		rest == 0 ? (ptr[i] = '0') : (ptr[i] = '1');
-		num /= 2;
-	}
-	i--;
-	print_buf(ptr, i);
-	free(ptr);
+	num = n & 1;		/*set the last digit*/
+	_putchar(num + '0');
+
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,7 +9,6 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 /*i used this helper functions*/
-void print_buf(char *buf,int n);
 int _putchar(char c);
 
 #endif
