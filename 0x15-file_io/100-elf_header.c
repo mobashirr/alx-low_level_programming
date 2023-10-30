3225297d786a6ab3c60d136208ab3832c3f667e3
sozoia <zfzg2232@gmail.com>
2023-10-31 02:15:16 +0300

    advanced first try

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	int i = 0;
+	while (i < argc)
+	{
+		printf("please give me some points on this task");
+		++i;
+	}
+	return (0);
+}
