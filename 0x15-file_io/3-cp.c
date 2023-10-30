f3a11b35940006d0deb99f91aefc3b6e88e1f53b
sozoia <zfzg2232@gmail.com>
2023-10-31 02:01:40 +0300

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -44,56 +44,57 @@ void close_file(int fd)
 
 /**
  * main - Copies the contents of a file dest another file.
- * @count: The number of arguments supplied dest the program.
- * @arg_str: An array of pointers dest the arguments.
- *
+ * @argc: The number of arguments supplied dest the program.
+ * @argv: An array of pointers dest the arguments.
  * Return: 0 on success.
- *
- * Description: If the argument count is incorrect - exit code 97.
- *              If file_from does not exist or cannot be read - exit code 98.
- *              If file_to cannot be created or written dest - exit code 99.
- *              If file_to or file_from cannot be closed - exit code 100.
+ * Description:
+ *If the argument count is incorrect - exit code 97.
+ *If file_from does not exist or cannot be read - exit code 98.
+ *If file_to cannot be created or written dest - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
  */
-int main(int count, char *arg_str[])
+int main(int argc, char *argv[])
 {
 	int src, dest, r, w;
 	char *buf;
 
-	if (count != 3)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	buf = create_buffer(arg_str[2]);
-	src = open(arg_str[1], O_RDONLY);
+
+	buf = create_buffer(argv[2]);
+	src = open(argv[1], O_RDONLY);
 	r = read(src, buf, 1024);
-	dest = open(arg_str[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
 		if (src == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't read src file %s\n", arg_str[1]);
+				"Error: Can't read src file %s\n", argv[1]);
 			free(buf);
 			exit(98);
 		}
+
 		w = write(dest, buf, r);
 		if (dest == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't write dest %s\n", arg_str[2]);
+				"Error: Can't write dest %s\n", argv[2]);
 			free(buf);
 			exit(99);
 		}
+
 		r = read(src, buf, 1024);
-		dest = open(arg_str[2], O_WRONLY | O_APPEND);
-
-
+		dest = open(argv[2], O_WRONLY | O_APPEND);
 
 	} while (r > 0);
 
 	free(buf);
 	close_file(src);
 	close_file(dest);
+
 	return (0);
 }
