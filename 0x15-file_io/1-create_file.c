7346f05430ef6da9a210aa1d2df9e39c8281498d
sozoia <zfzg2232@gmail.com>
2023-10-31 01:35:42 +0300

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_file -creates an array of chars, and initializes
+ * @text_content: is a NULL terminated string to write to the file
+ * @filename: is the name of the file to create
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int i, j, length = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (length = 0; text_content[length];)
+			length++;
+	}
+
+	i = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	j = write(i, text_content, length);
+
+	if (i == -1 || j == -1)
+	return (-1);
+
+	close(i);
+	return (1);
+}
