b2ebc6ac2e8c50ae65270765ef19c2d51689e5a9
sozoia <zfzg2232@gmail.com>
2023-12-07 13:37:26 +0300

    style

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,18 +7,18 @@
 */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t num = 0;
+	size_t num = 0;
 
-    if (!h)
-        return (0);
-    while (h)
-    {
-        printf("%d\n",h->n);
-        ++num;
-        if (h->next)
-            h = h->next;
-        else
-            break;
-    }
-    return (num);
+	if (!h)
+		return (0);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		++num;
+		if (h->next)
+			h = h->next;
+		else
+			break;
+	}
+	return (num);
 }
