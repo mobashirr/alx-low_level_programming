cf4ac7425ea6aa3595e824175ea340226c20ce87
sozoia <zfzg2232@gmail.com>
2023-12-07 14:55:16 +0300

    style

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,30 +1,32 @@
 #include "lists.h"
 
 /**
- * 
- *
+ * get_dnodeint_at_index - get node at index
+ *@head: head
+ *@index: index
+ *Return: adress of wanted node
 */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-        unsigned int i = 0;
-        dlistint_t *wanted;
+		unsigned int i = 0;
+		dlistint_t *wanted;
 
-        if(!head)
-            return (NULL);
-        wanted = head;
+		if (!head)
+			return (NULL);
+			wanted = head;
 
-        while (wanted)
-        {
-            if (i == index)
-                return (wanted);
-            if(wanted->next)
-            {
-                wanted = wanted->next;
-                ++i;
-            }
-            else
-                break;
+		while (wanted)
+		{
+			if (i == index)
+				return (wanted);
+			if (wanted->next)
+			{
+				wanted = wanted->next;
+				++i;
+			}
+			else
+				break;
 
-        }
-        return (NULL);
+		}
+		return (NULL);
 }
