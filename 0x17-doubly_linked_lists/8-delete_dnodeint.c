5869a285c780be93fe555357e12f39561e39c5f8
sozoia <zfzg2232@gmail.com>
2023-12-07 22:04:12 +0300

    style

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -13,12 +13,12 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if ((*head) && index == 0)
 	{
-		temp = (*head)->next;
+		if ((*head)->next)
+			temp = (*head)->next;
 		free(*head);
 		(*head) = temp;
 		return (1);
 	}
-
 
 	if ((*head))
 		temp = (*head);
@@ -36,13 +36,13 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			temp->prev = temp2;
 			return (1);
 		}
-		if(temp->next)
+		if (temp->next)
 		{
-	   		temp = temp->next;
-		++i;
+			temp = temp->next;
+			++i;
 		}
 		else
-		break;
+			break;
 	}
 	return (-1);
 }
