53038533084e4cb87a6edf47c05b035a91bc7060
sozoia <zfzg2232@gmail.com>
2023-12-12 00:52:05 +0300

    giga challenge

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -O lib.so https://github.com/sozoia/alx-low_level_programming/raw/main/0x18-dynamic_libraries/mylib.so
-export LD_PRELOAD=/lib.so
+wget -P /fol https://github.com/sozoia/alx-low_level_programming/raw/main/0x18-dynamic_libraries/mylib.so
+export LD_PRELOAD=/fol/mylib.so
 
