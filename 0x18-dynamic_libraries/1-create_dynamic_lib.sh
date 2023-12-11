de01a897e86a142fe6e8c59d75435edbf67ae54c
sozoia <zfzg2232@gmail.com>
2023-12-11 15:20:51 +0300

    dll script

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,11 @@
-!#/bin/bash
-gcc -c *.c -fpic
-gcc -shared *.o -o liball.so
+#!/bin/bash
+
+# Compile all .c files into object files
+gcc -c -fpic *.c
+
+# Create the dynamic library
+gcc -shared -o liball.so *.o
+
+# Clean up the object files
+rm *.o
 
