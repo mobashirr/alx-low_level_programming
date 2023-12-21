2f4fb193e41eb259d7f2aba9abae9291ec998eca
sozoia <zfzg2232@gmail.com>
2023-12-21 18:30:06 +0300

    hash table set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -5,9 +5,33 @@
  * @ht: hash table
  * @key: the key we want to add or update
  * @value: new value 
- * Return: 0 if success else 1
+ * Return: 1 if successeed else 0
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	hash_node_t *new = NULL;
+	unsigned int ind, size_hash;
 
+	if (!key || !value || !ht)
+		return (0);
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+		return (0);
+
+	new->key = key;
+	new->value = value;
+	new->next = NULL;
+
+	size_hash = ht->size;	/*size of hash table*/
+	ind = key_index(key, size_hash); /*get the index for key*/
+
+	if (ht->array[ind])	
+	{	/*if already key there we handle it using chaining method*/
+		new->next = ht->array[ind];
+		ht->array[ind] = new;
+	}
+	else
+		ht->array[ind] = new;
+
+	return (1);
 }
