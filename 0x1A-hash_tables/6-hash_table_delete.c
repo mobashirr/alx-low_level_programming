e74ac6ff03fb623d3e8477e2853161ac41ec436d
sozoia <zfzg2232@gmail.com>
2023-12-22 18:56:50 +0300

    implement del hash_table func

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_delete - delete hash table
+ * @ht: hash table
+*/
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node, *temp;
+
+	for (i = 0; i < ht->size; ++i)
+	{
+		node = ht->array[i];
+		while (node)
+		{	/*to free any linked list in the chain if there:*/
+			free(node->key);
+			free(node->value);
+			temp = node->next;
+			free(node);	/*free current node*/
+			node = temp;	/*move to next node if there*/
+		}
+	}
+	free(ht->array);	/*free the array*/
+	free(ht);			/*free the hash table ptr*/
+}
