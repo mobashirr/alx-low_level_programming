e74bd3d990cf5b50978e5961e4847bc9c740858a
sozoia <zfzg2232@gmail.com>
2023-12-22 17:58:59 +0300

    style betty

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,31 +11,30 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	/*we need to get value of key:*/
 	int ind = -1;
 	char *array = NULL;
-	char * wrong = "(null)";
 
 	if (!ht || !key)
-		return (wrong);
+		return (NULL);
 
 	/*first get teh index using my key_index:*/
-	
+
 	ind = key_index((const unsigned char *)key, ht->size);
-	
-	
+
+
 	if (ind != -1 && ht->array[ind] && ht->array[ind]->key)
 	{
 		/*now i have the index i can access the node of my key:*/
 	if (ht->array[ind]->value != NULL)
 	{
-    	array = (char *)ht->array[ind]->value;
-    	return (array);
+		array = (char *)ht->array[ind]->value;
+		return (array);
 	}
 	else
 	{
 
-    	return NULL;
+		return (NULL);
 	}
 
 	}
 	else
-		return (wrong);
+		return (NULL);
 }
