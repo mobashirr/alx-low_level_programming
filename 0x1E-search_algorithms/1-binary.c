cb3a768c205dd2256f8bf9e22514b8d93c6d6ef7
sozoia <zfzg2232@gmail.com>
2024-07-18 20:52:37 +0300

    task 1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - binary search algorithm
+ * @array: pointer to array of type int
+ * @size: size of the array
+ * @value: we want to find in the array
+ *
+ * Return: int
+ */
+int binary_search(int *array, size_t size, int value)
+{
+    size_t i, j, k, med;
+
+    if (array == NULL || size == 0)
+        return (-1);
+
+    i = 0;
+    j = size - 1;
+
+    while (i <= j)
+    {
+        med = (i + j) / 2;
+        printf("Searching in array: ");
+        for (k = i; k <= j; k++)
+        {
+            printf("%d", array[k]);
+            if (k < j)
+                printf(", ");
+        }
+        printf("\n");
+
+        if (array[med] == value)
+            return (med);
+        else if (array[med] > value)
+            j = med - 1;
+        else
+            i = med + 1;
+    }
+
+    return (-1);
+}
